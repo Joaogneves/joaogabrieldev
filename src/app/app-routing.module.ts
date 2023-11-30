370a5f02737f823a001683ef530e72356d087ceb
João Gabriel <joaogabriel443@gmail.com>
2023-11-30 12:29:27 -0300

    Criação da página projects

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -5,6 +5,7 @@ const routes: Routes = [
   { path: 'home', loadChildren: () => import('./home/home.module').then(m => m.HomeModule) },
   { path: '', loadChildren: () => import('./home/home.module').then(m => m.HomeModule) },
   { path: 'about', loadChildren: () => import('./about/about.module').then(m => m.AboutModule) },
+  { path: 'projects', loadChildren: () => import('./projects/projects.module').then(m => m.ProjectsModule) },
 
 ];
 
